31f22866ef7b1589b48445986ee7dd3e39196475
aurel fontaine <ajfame11@gmail.com>
2020-03-30 16:14:18 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,13 +1,9 @@
 def oxford_comma(array)
   if array.size == 1
-    return array[0]
+    array[0]
   elsif array.size == 2
-  array.join(" and ")
+    array.join(" and ")
   elsif array.size == 3
-  n = array[-1]
-  array.join(" , ")
-  array.pop
-  array << "and" "n"
-  
+    array.
 end
 end
